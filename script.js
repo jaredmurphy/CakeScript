f1ac11cc230b5c57c58c25dbe7d1a56f4fc66463
Jared <jaredcmurphy@gmail.com>
2016-03-14 21:49:19 -0400

    updated index.html

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 $(document).ready(function() {
   console.log("script.js loaded");
 
-  var arr = [1,2,3,4,5]
-  $('#click').on('click', function() {
-      $('body').append('<p>sampled array index = ' + arr.sample() + ' </p>');
+  var arr = [1,2,3,4,5];
+  $('#arraySampleClick').on('click', function() {
+      $('#arraySampleShow').append('<p>sampled array index = ' + arr.sample() + ' </p>');
       //console.log('sampled array index = ' + arr.sample());
+  });
+
+  var str = "Hello, world!";
+  $('#stringReverseClick').on('click', function() {
+    $('#stringReverseShow').append('<p>reversed string = ' + str.reverse() + ' </p>');
   });
 });
