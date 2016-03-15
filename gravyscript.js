8eb35532d5a468da051ff713fd438efe72cb2613
Jared <jaredcmurphy@gmail.com>
2016-03-14 22:40:52 -0400

    adds array.shuffle

--- gravyscript.js
+++ gravyscript.js
@@ -28,6 +28,18 @@ $(document).ready(function() {
     return this[randomIndex];
   }
 
+  // array.shuffle();
+  Array.prototype.shuffle = function() {
+    for (var i = this.length-1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+    }
+    return this;
+  }
+
+
  // STRING METHODS
   
   // string.reverse();
